ec823cdc61ea9405b0cf5415f1283dd5587d3c7c
Sergey Piunov <piunov92@gmail.com>
2023-05-21 21:26:39 +0300

    add UserService

---
+++ src/main/java/web/service/UserService.java
@@ -0,0 +1,9 @@
+package web.service;
+
+import web.models.User;
+
+import java.util.List;
+
+public interface UserService {
+    List<User> getUsers();
+}
---
+++ src/main/java/web/service/UserServiceImpl.java
@@ -0,0 +1,26 @@
+package web.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import web.dao.UserDao;
+import web.models.User;
+
+import java.util.List;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private final UserDao userDao;
+
+    @Autowired
+    public UserServiceImpl(UserDao userDao) {
+        this.userDao = userDao;
+    }
+
+    @Override
+    @Transactional
+    public List<User> getUsers() {
+        return userDao.getUsers();
+    }
+}
