1fb4497844a034bf0342e8b71035699288a59564
Sergey Piunov <piunov92@gmail.com>
2023-05-25 01:33:30 +0300

    update UserDaoImpl

--- src/main/java/web/dao/UserDaoImpl.java
+++ src/main/java/web/dao/UserDaoImpl.java
@@ -1,42 +1,42 @@
 package web.dao;
 
-import org.hibernate.SessionFactory;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 import web.models.User;
-
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
 public class UserDaoImpl implements UserDao {
 
-    private final SessionFactory sessionFactory;
-
-    @Autowired
-    public UserDaoImpl(SessionFactory sessionFactory) {
-        this.sessionFactory = sessionFactory;
-    }
-
+    @PersistenceContext(unitName = "entityManagerFactory")
+    private EntityManager entityManager;
+    
     @Override
     public List<User> getUsers() {
-        return sessionFactory.getCurrentSession().createQuery("from User", User.class).getResultList();
+        return entityManager.createQuery("from User", User.class).getResultList();
     }
 
     @Override
     public void saveUser(User user) {
-            sessionFactory.getCurrentSession().saveOrUpdate(user);
+        User existingUser = getUserById(user.getId());
+        if(existingUser == null) {
+            entityManager.persist(user);
+        } else {
+            existingUser.setName(user.getName());
+            existingUser.setSurname(user.getSurname());
+            existingUser.setAge(user.getAge());
+        }
     }
 
     @Override
     public User getUserById(int id) {
-        return sessionFactory.getCurrentSession().get(User.class, id);
+        return entityManager.find(User.class, id);
     }
 
     @Override
     public void deleteUser(int id) {
-        sessionFactory.getCurrentSession()
-                .createQuery("delete from User where id = :userId")
-                .setParameter("userId", id)
-                .executeUpdate();
+        entityManager.remove(getUserById(id));
+        entityManager.flush();
     }
 }
